178c6c87bd640d598142588268cfe18e03c42894
dauqu <sujeetsingh734321@gmail.com>
2023-01-21 12:57:25 +0530

    chng

--- src/components/Body/LatestNews.jsx
+++ src/components/Body/LatestNews.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import bg5 from "../assets/images/bg5.jpg";
-import bg6 from "../assets/images/bg6.jpg";
-import bg7 from "../assets/images/bg7.jpg";
+import bg5 from "../assets/images/web4.webp";
+import bg7 from "../assets/images/web5.webp";
+import bg6 from "../assets/images/web6.webp";
 
 function LatestNews() {
   return (
@@ -17,8 +17,13 @@ function LatestNews() {
             </div>
             <div className="mt-4 mb-4 font-bold text-[22px]">Dauqu Panel</div>
             <div className="leading-6 text-justify">
-              Duis autem vel eum iriure dolor in hendrerit in vulpu tate velit
-              esse molestie consequat.
+              Dauqu Panel is a web panel that allows you to manage your website
+              easily and quickly. It allows to host your website on the cloud
+              easily. Click
+              <a href="https://dauqu-cp.vercel.app/">
+                <span className="font-bold underline italic"> here </span>
+              </a>
+              to visit Dauqu Panel.
             </div>
           </div>
           <div className="md:w-[350px] w-auto md:m-4 m-auto md:pt-0 pt-8">
@@ -27,20 +32,23 @@ function LatestNews() {
             </div>
             <div className="mt-4 mb-4 font-bold text-[22px]">Dauqu Website</div>
             <div className="leading-6 text-justify">
-              Duis autem vel eum iriure dolor in hendrerit in vulpu tate velit
-              esse molestie consequat.
+              From Dauqu website you can easily purchase our products and
+              services and also you can get more information about our products
+              and services. Click
+              <a href="https://cp-dauqu-frontend.vercel.app/">
+                <span className="font-bold underline italic"> here </span>
+              </a>
+              to visit Dauqu Website.
             </div>
           </div>
           <div className="md:w-[350px] w-auto md:m-4 m-auto md:pt-0 pt-8">
             <div className="w-full">
               <img src={bg7} alt="" srcset="" />
             </div>
-            <div className="mt-4 mb-4 font-bold text-[22px]">
-              Best Renovation
-            </div>
+            <div className="mt-4 mb-4 font-bold text-[22px]">Best Security</div>
             <div className="leading-6 text-justify">
-              Duis autem vel eum iriure dolor in hendrerit in vulpu tate velit
-              esse molestie consequat.
+              We provide the best security for your website. Alogn this we also provide the budget plan  for your website .
+              It is very easy to use and manage your website. 
             </div>
           </div>
         </div>
